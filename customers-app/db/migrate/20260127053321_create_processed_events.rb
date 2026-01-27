9b90ef1714625a6fa4ae435b7d5af7db3983d0f1
Andrea Fuentes <mfuentescastellanos@gmail.com>
2026-01-27 00:58:44 -0500

    Add customer model, processed events and seeds

---
+++ customers-app/db/migrate/20260127053321_create_processed_events.rb
@@ -0,0 +1,12 @@
+class CreateProcessedEvents < ActiveRecord::Migration[8.1]
+  def change
+    create_table :processed_events do |t|
+      t.string :event_id, null: false
+      t.datetime :processed_at, null: false, default: -> { "CURRENT_TIMESTAMP" }
+
+      t.timestamps
+    end
+
+    add_index :processed_events, :event_id, unique: true
+  end
+end
